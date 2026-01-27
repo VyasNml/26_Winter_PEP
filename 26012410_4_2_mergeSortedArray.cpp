f22ff98777a9ca21b96ff2f4304524f1d8253333
VyasNml <vyasnml@gmail.com>
2026-01-27 10:17:12 +0530

    mergeToSortedArray

--- 26012410_4_2_mergeSortedArray.cpp
+++ 26012410_4_2_mergeSortedArray.cpp
@@ -2,7 +2,30 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+//Leetcode Solution
+class Solution {
+public:
+    void merge1(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+	    int i=m-1,j=n-1,k=m+n-1;
+	    while(j>=0){
+		    if(i>=0 && nums1[i] > nums2[j]){
+				  nums1[k--] > nums1[i--];
+			  }else{
+				  nums1[k--] = nums2[j--];
+			  }
+		  }
+    }
+
+    void merge2(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i=0;i<n;i++){
+            nums1[m++]=nums2[i];
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+};
 
 void VecIn(vector<int> &v, int n) {
     v.resize(n);
--- 26012716_4_1_remove-duplicates-from-sorted-array.cpp
+++ 26012716_4_1_remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,18 @@
 //https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=problem-list-v2&envId=arrayhttps://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=problem-list-v2&envId=array
 
+//10
+// 0 0 1 1 1 2 2 3 3 4
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int removeDuplicates(vector<int>& nums) {
-        int j=0,curr=nums[0];
-        for(int i=0;i<nums.size();i++){
-            if(curr!=nums[i]){
+        int j=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[j]!=nums[i]){
                 nums[++j]=nums[i];
-                curr = nums[i];
             }
         }
         return j+1;
@@ -22,22 +25,23 @@ void VecIn(vector<int> &v, int n) {
     }
 }
 
-void VecOut(const vector<int> &v) {
-    for (int x : v) {
-        cout << x << ' ';
+void VecOut(vector<int> &v,int l) {
+    for (int i=0;i<l;i++) {
+        cout << v[i] << ' ';
     }
     cout << endl;
 }
 
 int main() {
     int n;
+    cout << "Provide Inputs\nn: ";
     cin >> n;
+    cout  << endl;
 
     vector<int> v;
     VecIn(v, n);
-    VecOut(v);
-
-    
+    int l = removeDuplicates(v);
+    VecOut(v,l);
 
     return 0;
 }
