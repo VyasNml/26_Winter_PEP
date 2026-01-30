696a5b1366324a4fd4f3a7c6775c65a29d8e65d0
VyasNml <vyasnml@gmail.com>
2026-01-30 19:02:48 +0530

    7.4 Recursive Binary Search

--- DSA/Recursion/26013032_7_6_ArrayRecursion.cpp
+++ DSA/Recursion/26013032_7_6_ArrayRecursion.cpp
@@ -21,6 +21,26 @@ bool isSorted(int arr[], int size){
     return isSorted(arr+1,size-1);
 }
 
+int ArrSum(int *arr,int n){
+    if(n==0)return arr[n];
+    return arr[n]+ArrSum(arr,n-1);
+}
+
+int LinearSearch(int arr[],int key,int size){
+    if(size==0)return false;
+    if(arr[0]==key)return true;
+    else return LinearSearch(arr+1,key,size-1);
+}
+
+int BinarySearch(int arr[],int key,int l,int r){
+    // return index
+    int m = (l+r)/2;
+    if(arr[m]==key)return m;
+    else if(arr[m]<key) return BinarySearch(arr,key,m+1,r);
+    else return BinarySearch(arr,23,l,m-1);
+    return -1;
+}
+
 int main() {
     // cin >> n;
     // int arr[n];
@@ -28,7 +48,9 @@ int main() {
     int n = 5;
     int arr[5] = {5,8,3,23,12};
 
-    cout << isSorted(arr,n);
+    cout << LinearSearch(arr,23,n);
+
+    // cout << BinarySearch(arr,23,0,n-1);
 
     
 
