8a135cdf48b3312ea81cd37c93b8baee3b7ff29e
VyasNml <vyasnml@gmail.com>
2026-02-12 11:37:33 +0530

    O. Fibonacci

---
+++ Practice/O.cpp
@@ -0,0 +1,20 @@
+// O. Fibonacci
+
+// https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/O
+
+#include <iostream>
+using namespace std;
+
+int Fibo(int n){
+    if(n<=1) return 0;
+    if(n==2) return 1; 
+    return Fibo(n-1)+Fibo(n-2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << Fibo(n);
+    
+    return 0;
+}
