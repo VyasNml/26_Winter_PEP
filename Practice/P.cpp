66c387e60da4e146690246c48245ccb076254ad1
VyasNml <vyasnml@gmail.com>
2026-02-12 15:53:50 +0530

    P. Log_2(n)

--- Practice/P.cpp
+++ Practice/P.cpp
@@ -1,2 +1,20 @@
 // P. Log2
 
+// https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/P
+
+#include <iostream>
+using namespace std;
+
+int solution(long long n){
+    if(n<=1)return 0;
+    if(n==2)return 1;
+    return 1+solution(n/2);
+}
+
+int main() {
+    long long n;
+    cin >> n;
+    cout << solution(n);
+    
+    return 0;
+}
